6dcb87171d8fa55a5b599a4016b64842733b523f
Tricia <tricianghiem@gmail.com>
2022-07-10 20:46:42 -0500

    initialized npm and created route to /notes

--- server.js
+++ server.js
@@ -8,3 +8,14 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// post routes
+
+
+// get routes 
+
+app.get("/notes", (req, res) => {
+    res.sendFile("./public/notes.html")
+});
+
+app.listen(PORT, () => console.log("listening on port " + PORT));
