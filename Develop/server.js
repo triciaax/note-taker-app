2d11943d2f457d8b114a69739cea61f790b6f33b
Tricia <tricianghiem@gmail.com>
2022-07-10 21:01:27 -0500

    moved server file into Develop folder

--- Develop/server.js
+++ Develop/server.js
@@ -16,14 +16,17 @@ app.use(express.json());
 
 // for the /notes endpoint, return the notes html file
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 // API route to return the db.json file
 app.get("/api/notes", (req, res) => {
-    res.sendFile(__dirname, "/Develop/db/db.json");
+    res.sendFile(__dirname, "/db/db.json");
 })
 
-
+// for all other urls, return the index file
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+})
 
 app.listen(PORT, () => console.log("listening on port " + PORT));
