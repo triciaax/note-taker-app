25894442afeaebcc1c7c6dbdf32b6144869b046f
Tricia <tricianghiem@gmail.com>
2022-07-10 21:17:23 -0500

    added route to save notes

--- Develop/server.js
+++ Develop/server.js
@@ -11,6 +11,18 @@ app.use(express.json());
 
 // post routes
 
+// to create a new note, post to /api/notes
+app.post('/api/notes', (req, res) => {
+    let note = req.body;
+    let noteList = JSON.parse(fs.readFileSync("./db/db.json"));
+    let length = noteList.length;
+
+    // assign the note an ID equal to the length of the list
+    note.id = length;
+    noteList.push(note);
+    fs.writeFileSync("./db/db.json", JSON.stringify(noteList));
+    res.json(noteList);
+})
 
 // get routes 
 
